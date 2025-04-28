cb66d3bba2367933b4968f6f7d0b731468f13847
Théo <t.knopfer@gmail.com>
2025-04-28 11:49:14 +0200

    correctif 2

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+const burger = document.querySelector('.burger'); // Cherche le bouton burger
+const nav = document.querySelector('nav ul'); // Cherche mon menu nav ul
+
+burger.addEventListener('click', () => { // Quand je clique sur le "bouton",
+//  ajoute ou enlève la classe open sur nav ul
+
+  nav.classList.toggle('open'); // ajoute "dynamiquement la classe .open"
+});
+
 //Carroussel
 document.addEventListener('DOMContentLoaded', () => {
   const container = document.querySelector('#portfolio .carroussel-container');
